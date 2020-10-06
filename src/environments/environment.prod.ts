9cf034c3c40844f3a7f5c4955baa2757b786107a
Evgeniy_Medvedev <Evgenii-Medvedev@list.ru>
2020-10-06 16:11:50 +0300

    first

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyByQ3HDzzVKlvJ4M2bfB_xygH4ZdcJYZzY',
+  fbDbUrl: 'https://angular-blog-a5a54.firebaseio.com'
+};
